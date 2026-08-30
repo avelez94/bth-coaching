2f12aa3cd41b893555cfd4781d7304b817de0980
avelez94 <alante.v@gmail.com>
2026-08-29 20:47:53 -0400

    Fix component import casing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Nav from './components/Nav'
-import Footer from './components/Footer'
+import Nav from './components/nav'
+import Footer from './components/footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
